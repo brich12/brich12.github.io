4915c1d7d710dcdafa9676d355d35dc6d798ba9c
BrandonRichOG <brandon.rich@ordergroove.com>
2026-02-26 16:56:44 -0800

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'cribbage-v10';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './app_icon2.png'
+];
+
+// Install: pre-cache all app assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: remove any old caches from previous versions
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
